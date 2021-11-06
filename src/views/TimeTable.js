2f9dd34543c05b62a14c26b14acb823d9c205c09
nicole <nicole_czy@hotmail.com>
2021-11-06 21:35:06 +0800

    add student calendar

--- src/views/TimeTable.js
+++ src/views/TimeTable.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 
 // reactstrap components
 import {
@@ -13,18 +16,56 @@ import {
 
 // core components
 import PanelHeader from "components/PanelHeader/PanelHeader.js";
-
+//add content to tiles
+const LectureDate = [
+  new Date(2021, 11, 1),
+  new Date(2021, 11, 5),
+];
 function TimeTable() {
- 
+  const [date, setDate] = useState(new Date());
   return (
     <>
-      <PanelHeader size="sm" />
+      <PanelHeader size="sm"/>
+      <div className='content'>
+        <h1 className='text-center'>-</h1>
+        <div className='calendar-container'>
+          <Calendar 
+          style={{ height: 600, width: 400 } }
+          onChange={setDate} 
+          value={date} 
+          locale="en-GB"
+          selectRange={true}
+          tileContent={
+            ({ activeStartDate, date, view }) => {
+              return view === 'month' && date.getDay() === 1
+              ? <p onMouseEnter={
+                  console.log('hi')
+                  }>ELEC5620 Lecture</p>
+              : null
+            }
+          }
+          />
+        </div>
+        {date.length > 0 ? (
+          <p className='text-center'>
+            <span className='bold'>Start:</span>{' '}
+            {date[0].toDateString()}
+            &nbsp;|&nbsp;
+            <span className='bold'>End:</span> {date[1].toDateString()}
+          </p>
+        ):(
+          <p className='text-center'>
+            <span className='bold'>Default selected date:</span>{' '}
+            {date.toDateString()}
+          </p>
+        )}
+      </div>
       <div className="content">
         <Row>
           <Col xs={12}>
             <Card>
               <CardHeader>
-                <CardTitle tag="h4">Course Schedule</CardTitle>
+                <CardTitle tag="h4">Student Timetable</CardTitle>
               </CardHeader>
               <CardBody>
                  <Table responsive>
