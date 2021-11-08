f1f0e868c24bd7a09ab3e34aac6618f9941ef1f1
lliihao <lliihao@outlook.com>
2021-11-08 10:23:53 +0800

    user page

--- src/views/UserPage.js
+++ src/views/UserPage.js
@@ -30,135 +30,38 @@ import {
   Col,
 } from "reactstrap";
 
+import "./userPageStyle.css";
+
 // core components
 import PanelHeader from "components/PanelHeader/PanelHeader.js";
 
+  
 function User() {
+
+  const userInfo={
+    fristName:"John",
+    lastName:"Smith",
+    userImage:"smho0607.jpg",
+    userName:"smho0607",
+    introduce:"I major in data science and I have 3 years of programming experience, and now I’m doing an internship at Google.",
+    email:"smho0607@uni.sydney.edu.au",
+    address:"KingStreet 206, Newtown, Sydney, NSW",
+    postalCode:"20209",
+    country:"Japan",
+    major:"Master of Data Science"
+  };
+  
+  submitUserInfo=()=>{
+
+  }
+
   return (
     <>
       <PanelHeader size="sm" />
       <div className="content">
-        <Row>
-          <Col md="8">
-            <Card>
-              <CardHeader>
-                <h5 className="title">Edit Profile</h5>
-              </CardHeader>
-              <CardBody>
-                <Form>
-                  <Row>
-                    <Col className="pr-1" md="5">
-                      <FormGroup>
-                        <label>Company (disabled)</label>
-                        <Input
-                          defaultValue="Creative Code Inc."
-                          disabled
-                          placeholder="Company"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-1" md="3">
-                      <FormGroup>
-                        <label>Username</label>
-                        <Input
-                          defaultValue="michael23"
-                          placeholder="Username"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-1" md="4">
-                      <FormGroup>
-                        <label htmlFor="exampleInputEmail1">
-                          Email address
-                        </label>
-                        <Input placeholder="Email" type="email" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-1" md="6">
-                      <FormGroup>
-                        <label>First Name</label>
-                        <Input
-                          defaultValue="Mike"
-                          placeholder="Company"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-1" md="6">
-                      <FormGroup>
-                        <label>Last Name</label>
-                        <Input
-                          defaultValue="Andrew"
-                          placeholder="Last Name"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="12">
-                      <FormGroup>
-                        <label>Address</label>
-                        <Input
-                          defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                          placeholder="Home Address"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-1" md="4">
-                      <FormGroup>
-                        <label>City</label>
-                        <Input
-                          defaultValue="Mike"
-                          placeholder="City"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-1" md="4">
-                      <FormGroup>
-                        <label>Country</label>
-                        <Input
-                          defaultValue="Andrew"
-                          placeholder="Country"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-1" md="4">
-                      <FormGroup>
-                        <label>Postal Code</label>
-                        <Input placeholder="ZIP Code" type="number" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="12">
-                      <FormGroup>
-                        <label>About Me</label>
-                        <Input
-                          cols="80"
-                          defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                            that two seat Lambo."
-                          placeholder="Here can be your description"
-                          rows="4"
-                          type="textarea"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                </Form>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md="4">
+        {/* <Row> */}
+
+        <Col className ="card-middle" md="8">
             <Card className="card-user">
               <div className="image">
                 <img alt="..." src={require("assets/img/bg5.jpg").default} />
@@ -171,14 +74,15 @@ function User() {
                       className="avatar border-gray"
                       src={require("assets/img/mike.jpg").default}
                     />
-                    <h5 className="title">Mike Andrew</h5>
+                    <h5 className="title">{userInfo.fristName+" "+userInfo.lastName}</h5>
                   </a>
-                  <p className="description">michael24</p>
+                  {/* <p className="description">michael24</p> */}
                 </div>
                 <p className="description text-center">
-                  "Lamborghini Mercy <br />
+                  {userInfo.introduce}
+                  {/* "Lamborghini Mercy <br />
                   Your chick she so thirsty <br />
-                  I'm in that two seat Lambo"
+                  I'm in that two seat Lambo" */}
                 </p>
               </CardBody>
               <hr />
@@ -213,7 +117,136 @@ function User() {
               </div>
             </Card>
           </Col>
-        </Row>
+
+
+          <Col md="15">
+            <Card>
+              <CardHeader>
+                <h5 className="title">Edit Profile</h5>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                  <Row>
+                    <Col className="pr-1" md="5">
+                      <FormGroup>
+                        <label>Major (disabled)</label>
+                        <Input
+                          defaultValue={userInfo.major}
+                          disabled
+                          placeholder="Major"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="px-1" md="3">
+                      <FormGroup>
+                        <label>Username</label>
+                        <Input
+                          defaultValue={userInfo.userName}
+                          placeholder="Username"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-1" md="4">
+                      <FormGroup>
+                        <label htmlFor="exampleInputEmail1">
+                          Email
+                        </label>
+                        <Input defaultValue={userInfo.email} placeholder="Email" type="email" />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="pr-1" md="6">
+                      <FormGroup>
+                        <label>First Name</label>
+                        <Input
+                          defaultValue={userInfo.fristName}
+                          placeholder="first name"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-1" md="6">
+                      <FormGroup>
+                        <label>Last Name</label>
+                        <Input
+                          defaultValue={userInfo.lastName}
+                          placeholder="Last Name"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md="12">
+                      <FormGroup>
+                        <label>Address</label>
+                        <Input
+                          defaultValue={userInfo.address}
+                          placeholder="Home Address"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    {/* <Col className="pr-1" md="4">
+                      <FormGroup>
+                        <label>City</label>
+                        <Input
+                          defaultValue="Mike"
+                          placeholder="City"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col> */}
+                    <Col className="pl-1" md="4">
+                      <FormGroup>
+                        <label>Postal Code</label>
+                        <Input defaultValue={userInfo.postalCode} placeholder="ZIP Code" type="number" />
+                      </FormGroup>
+                    </Col>
+                    
+                    <Col className="px-1" md="4">
+                      <FormGroup>
+                        <label>Country</label>
+                        <Input
+                          defaultValue={userInfo.country}
+                          placeholder="Country"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    
+                  </Row>
+                  <Row>
+                    <Col md="12">
+                      <FormGroup>
+                        <label>About Me</label>
+                        <Input
+                          cols="80"
+                          defaultValue={userInfo.introduce}
+                          placeholder="Here can be your description"
+                          rows="4"
+                          type="textarea"
+                        />
+                      </FormGroup>
+                    </Col>
+
+                    <div>
+                    <small className="authorName">
+                      <Button onClick={this.submitUserInfo}>Submit</Button>
+                    </small>
+                  </div>
+                  </Row>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+          
+        {/* </Row> */}
       </div>
     </>
   );
